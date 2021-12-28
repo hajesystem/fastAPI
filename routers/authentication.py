ff587c87c87d80b602d5b41e95f4909091fff5c9
hajesystem <hajesystem@gmail.com>
2021-12-28 20:24:02 +0900

    JSONResponse

--- routers/authentication.py
+++ routers/authentication.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, status, Depends, Response
 from sqlalchemy.exc import SQLAlchemyError
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
-from starlette.responses import JSONResponse
+from fastapi.responses import JSONResponse
 from models.db_model import UserModel
 from models import db_session
 from services.jwt_token import refresh_token, encode_access_token, encode_refresh_token
